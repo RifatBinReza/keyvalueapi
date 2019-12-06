c9539af0f9566c58981e0a24243a2d5e1ac613f4
Emon Reza <rifatbinreza@gmail.com>
2019-12-06 16:59:22 +0600

    Add Joi for express api request validation. Validate /object post route for testing if it is configured properly.

--- controllers/key_value.js
+++ controllers/key_value.js
@@ -1,5 +1,25 @@
 const {models} = require('../model');
+const Joi = require('joi');
 
 exports.saveObject = (req, res)=>{
-  
+  const data = req.body;
+  // Validate our database schema with Joi
+  const schema = Joi.object().keys({
+    key: Joi.string().required(),
+    value: Joi.object(),
+  })
+  Joi.validate(data, schema, (err, value)=>{
+    if(err){
+      // send a 422 error response if validation fails
+      res.status(422).json({
+        status: 'error',
+        message: 'Invalid request data',
+        data: data
+      });
+    } else {
+      res.status(200).json({
+        status: 'success',
+      })
+    }
+  })
 }
