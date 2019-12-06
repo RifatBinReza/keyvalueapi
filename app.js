9589129c09edb6d474928a6ac4432e0aa877c64e
Emon Reza <rifatbinreza@gmail.com>
2019-12-06 15:35:37 +0600

    Initial boilerplate for rest api backend. Connect to postgres using sequelize.

---
+++ app.js
@@ -0,0 +1,72 @@
+
+/**
+ * Module dependencies.
+ */
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const chalk = require("chalk");
+const dotenv = require("dotenv");
+const errorHandler = require("errorhandler");
+const moment = require("moment");
+
+/**
+ * Load environment variables from .env file, where all the credentials are configured.
+ */
+// dotenv.load({ path: ".env" });
+
+/**
+ * Get the databse connection
+ */
+const { sequelize } = require("./model");
+
+/**
+ * Create Express server.
+ */
+const app = express();
+
+/**
+ * Express configuration.
+ */
+app.set("host", process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0");
+app.set("port", process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true,
+    parameterLimit: 50000
+  })
+);
+
+/**
+ * Error Handler.
+ */
+app.get("*", (req, res) => {
+  res.redirect("/");
+});
+
+if (process.env.NODE_ENV === "development") {
+  // only use in development
+  app.use(errorHandler());
+} else {
+  app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send("Server Error");
+  });
+}
+
+/**
+ * Connect the database and start Express server.
+ */
+sequelize.sync().then(() => {
+  let server = app.listen(app.get("port"), () => {
+    console.log(
+      "%s App is running at http://localhost:%d in %s mode",
+      chalk.green("✓"),
+      app.get("port"),
+      app.get("env")
+    );
+    console.log("  Press CTRL-C to stop\n");
+  });
+});
---
+++ model/index.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+/**
+ * Connect to the database instance
+ */
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: "postgres"
+  }
+);
+
+/**
+ * Get all the models for exporting
+ */
+const models = {
+
+};
+Object.keys(models).forEach(key => {
+  if ("associate" in models[key]) {
+    models[key].associate(models);
+  }
+});
+module.exports = { sequelize, models };
